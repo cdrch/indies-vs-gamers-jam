e4377c557d5c8ce74d6bde429a8d4065d85f9514
Daniel Alexandru Radu <danielalexandru.radu@gmail.com>
2015-07-18 06:22:23 +0300

    Shooting is good
    Implemented a single type of weapon
    *todo: should see how to separate weapons from player file*

--- Player.js
+++ Player.js
@@ -1,4 +1,39 @@
 var that2;
+var Weapon = {};
+
+Weapon.SingleBullet = function (game) {
+
+    Phaser.Group.call(this, game, game.world, 'Single Bullet', false, true, Phaser.Physics.ARCADE);
+
+    this.nextFire = 0;
+    this.bulletSpeed = 600;
+    this.fireRate = 100;
+
+    for (var i = 0; i < 64; i++)
+    {
+        this.add(new Bullet(game, 'bullet'), true, true);
+    }
+
+    return this;
+
+};
+
+
+Weapon.SingleBullet.prototype = Object.create(Phaser.Group.prototype);
+Weapon.SingleBullet.prototype.constructor = Weapon.SingleBullet;
+
+Weapon.SingleBullet.prototype.fire = function (source) {
+
+    if (this.game.time.time < this.nextFire) { return; }
+
+    var x = source.sprite.x + 10;
+    var y = source.sprite.y + 10;
+
+    this.getFirstExists(false).fire(x, y, 0, this.bulletSpeed, source.playerTarget.x, source.playerTarget.y);
+
+    this.nextFire = this.game.time.time + this.fireRate;
+
+};
 
 var Player = function(game, posX, posY, imageName) {
 	that2 = this;
@@ -11,13 +46,6 @@ var Player = function(game, posX, posY, imageName) {
 
 	game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
     this.MAXSPEED = 500; // player's maximum velocity
-    //this.ACCELERATION = 1500; // player's maximum acceleration
-    //this.DRAG = 1000; // player's drag
-    //this.INPUTMODIFYFACTOR = 0.7071;
-
-    //this.sprite.body.maxVelocity.setTo(this.MAXSPEED, this.MAXSPEED); // x, y
-
-    //this.sprite.body.drag.setTo(this.DRAG, this.DRAG); // x, y
 
     this.sprite.body.collideWorldBounds = true;
 
@@ -25,19 +53,24 @@ var Player = function(game, posX, posY, imageName) {
     this.playerTarget = game.add.sprite(game.input.mousePointer.x, game.input.mousePointer.y, 'crosshair');
     this.playerTarget.anchor.set(0.5);
 
-    bullets = game.add.group();
-	bullets.enableBody = true;
-	bullets.physicsBodyType = Phaser.Physics.ARCADE;
-
-	bullets.createMultiple(50, 'bullet');
-	bullets.setAll('checkWorldBounds', true);
-	bullets.setAll('outOfBoundsKill', true);
-
     this.setControls(game);
 
-    //game.camera.follow(this.sprite);
-    this.sprite.body.velocity.x = 0;
-    this.sprite.body.velocity.y = 0;
+    game.camera.follow(this.sprite);
+
+
+
+    this.weapons = [];
+    this.currentWeapon = 0;
+    this.weaponName = null;
+
+    this.weapons.push(new Weapon.SingleBullet(game));
+
+    
+    
+    for (var i = 1; i < this.weapons.length; i++)
+    {
+        this.weapons[i].visible = false;
+    }
 
 };
 
@@ -50,10 +83,6 @@ Player.prototype.setControls = function(game) {
 };
 
 Player.prototype.movePlayer = function(game) {
-    // var xInput = this.leftControl.isDown ? -1 : 0;
-    // xInput += this.rightControl.isDown ? 1 : 0;
-    // var yInput = this.upControl.isDown ? -1: 0;
-    // yInput += this.downControl.isDown ? 1: 0;
     
     var xInput = 0;
     var yInput = 0;
@@ -83,23 +112,6 @@ Player.prototype.movePlayer = function(game) {
 
     this.sprite.body.velocity.x = xInput * this.MAXSPEED;
     this.sprite.body.velocity.y = yInput * this.MAXSPEED;
-/*
-    var diagonal = (xInput !== 0 && yInput !== 0) ? true : false;
-
-    var xMovement;
-    var yMovement;
-
-    if (diagonal)
-    {
-        xMovement = xInput * this.MAXSPEED * (Math.sqrt(xInput * xInput + yInput * yInput));
-        yMovement = yInput * this.MAXSPEED * (Math.sqrt(xInput * xInput + yInput * yInput));
-    }
-    else
-    {
-        xMovement = xInput * this.MAXSPEED;
-        yMovement = yInput * this.MAXSPEED;
- }
-*/
 
 };
 
@@ -108,7 +120,7 @@ Player.prototype.playerShoot = function(game) {
     if(game.input.activePointer.isDown || this.spaceControl.isDown)
     {
     	//handle the bullet creation and firing
-    	this.fire(game);
+    	this.weapons[this.currentWeapon].fire(this);
    }
 };
 
@@ -117,20 +129,6 @@ Player.prototype.updatePlayerTarget = function(game) {
     	this.playerTarget.y = game.camera.y + game.input.mousePointer.y;
 
  	};
-
-
-Player.prototype.fire = function(game) {
-    if (game.time.now > game.player.nextFire && bullets.countDead() > 0)
-    {
-	    game.player.nextFire = game.time.now + game.player.fireRate;
-
-	    var bullet = bullets.getFirstDead();
-
-	    bullet.reset(game.player.sprite.x, game.player.sprite.y);
-
-	    bullet.rotation = game.physics.arcade.moveToPointer(bullet, 300);
-    }
-};
 
 Player.prototype.update = function(game) {
 
