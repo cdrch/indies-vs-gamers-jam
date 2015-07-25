3546ab1b6f93d6ee1353ee6c914a8b39bb3968d6
Carter David Henning <carterdavidhenning@yahoo.com>
2015-07-25 11:12:29 -0400

    Basic loading bar added - note that it does not account for the time it takes to decode audio files.

--- Preloader.js
+++ Preloader.js
@@ -2,7 +2,7 @@
 ArcaneArcade.Preloader = function (game) {
 
 	//this.background = null;
-	//this.preloadBar = null;
+	this.preloadBar = null;
 
 	this.ready = false;
 
@@ -15,12 +15,12 @@ ArcaneArcade.Preloader.prototype = {
 		//	These are the assets we loaded in Boot.js
 		//	A nice sparkly background and a loading progress bar
 		//this.background = this.add.sprite(0, 0, 'preloaderBackground');
-		//this.preloadBar = this.add.sprite(300, 400, 'preloaderBar');
+		this.preloadBar = this.add.sprite(256, 384, 'preloaderBar');
 
 		//	This sets the preloadBar sprite as a loader sprite.
 		//	What that does is automatically crop the sprite from 0 to full-width
 		//	as the files below are loaded in.
-		//this.load.setPreloadSprite(this.preloadBar);
+		this.load.setPreloadSprite(this.preloadBar);
 
 		//	Here we load the rest of the assets our game needs.
 		//	As this is just a Project Template I've not provided these assets, swap them for your own.
@@ -104,7 +104,7 @@ ArcaneArcade.Preloader.prototype = {
 	create: function () {
 
 		//	Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
-		//this.preloadBar.cropEnabled = false;
+		this.preloadBar.cropEnabled = false;
 		
 		this.sound.setDecodedCallback([ 'titleSong', 'characterTheme', 'endOfGameSong', 'finalBossTheme', 'gameOverSong',
 			 'ghostTheme', 'magicalGirlThemeOne', 'magicalGirlThemeTwo', 'sadThemeOne', 'sadThemeTwo',
